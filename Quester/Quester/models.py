08efe8a5733f765ead9e07e21a3d966c69ac1e94
Tahmidur_Rabb <tr1476@nyu.edu>
2023-09-04 19:21:18 -0400

    Functionality

---
+++ Quester/Quester/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+
+    title = models.CharField(max_length= 100, null= True)
+
+    content = models.CharField(max_length=1000, null=True, blank= True)
+
+    date_posted = models.DateTimeField(auto_now_add=True, null=True)
+
+    user = models.ForeignKey(User, max_length= 10, on_delete= models.CASCADE, null = True)
